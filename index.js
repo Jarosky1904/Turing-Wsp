c9fb75785cdce5c02173833a3d3071373fc05f2e
Jarosky1904 <danijara1904@gmail.com>
2025-05-06 19:49:58 -0400

    Correccion de nombre

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ client.on('ready', async () => {
         // Guardar config.json
         const configData = {
             usuario: miNombre,
-            usuario2: nombreOtro
+            contacto: nombreOtro
         };
     
         fs.writeFileSync(
